cabe966b697899d81b7ffa54e10f2be009a2a603
jimafisk <jimafisk@gmail.com>
2020-11-03 13:25:02 -0500

    Initial commit.

---
+++ layout/global/head.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let title;
+</script>
+
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width,initial-scale=1'>
+
+  <title>{ title }</title>
+
+  <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg">
+  <link rel='stylesheet' href='/spa/bundle.css'>
+</head>
